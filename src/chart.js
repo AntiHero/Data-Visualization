c5187ccbec4cfbb794b865e6a143e4ac6f646bdd
Aleksandr Schemelev <prophreaker@gmail.com>
2021-03-27 16:05:56 +0300

    feat: add css loader to webpack

--- src/chart.js
+++ src/chart.js
@@ -1,5 +1,6 @@
 import * as d3 from "d3";
 import { yAccessor, xAccessor } from './utils/accessors';
+import './styles/style.css';
 
 async function drawLineChart() {
   const dataset = await d3.json('./my_weather_data.json');
